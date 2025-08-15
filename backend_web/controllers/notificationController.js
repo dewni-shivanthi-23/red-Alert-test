06c758854b9a192fd6902b4f006df003e9ba9043
DewniAndradi <158011656+dewni-shivanthi-23@users.noreply.github.com>
2025-08-15 22:22:16 +0530

    Update notificationController.js

--- backend_web/controllers/notificationController.js
+++ backend_web/controllers/notificationController.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const Notification = require("../models/notification");
 const notificationHospital = require("../models/notificationHospital");
 const { Expo } = require('expo-server-sdk');
-const User = require("../../backend/models/User");
+const User = require("../../models/User");
 const notification = require("../models/notification");
 let expo = new Expo();
 
